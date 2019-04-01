f986881207b7760af014e1621237cc64bd697a6a
Aarsh Patel <32023476+aarsh2000@users.noreply.github.com>
2019-03-31 23:55:19 -0400

    Update protectCircle.js

--- project/protectCircle.js
+++ project/protectCircle.js
@@ -410,6 +410,76 @@ function removeSquares(){
 
     });
 
+    let pageWidth = window.innerWidth || document.body.clientWidth;
+    let treshold = Math.max(1,Math.floor(0.01 * (pageWidth)));
+    let touchstartX = 0;
+    let touchstartY = 0;
+    let touchendX = 0;
+    let touchendY = 0;
+    
+    const limit = Math.tan(45 * 1.5 / 180 * Math.PI);
+    const gestureZone = document.getElementById('modalContent');
+    
+    window.addEventListener('touchstart', function(event) {
+        touchstartX = event.changedTouches[0].screenX;
+        touchstartY = event.changedTouches[0].screenY;
+    }, false);
+    
+    window.addEventListener('touchend', function(event) {
+        touchendX = event.changedTouches[0].screenX;
+        touchendY = event.changedTouches[0].screenY;
+        handleGesture(event);
+    }, false);
+    
+    function handleGesture(e) {
+        let x = touchendX - touchstartX;
+        let y = touchendY - touchstartY;
+        let xy = Math.abs(x / y);
+        let yx = Math.abs(y / x);
+        if (Math.abs(x) > treshold || Math.abs(y) > treshold) {
+            if (yx <= limit) {
+                if (x < 0) {
+                    left = true;
+                    up=false;
+                    down=false;
+                    right = false;
+                } else {
+                    right = true;
+                    down=false;
+                    left=false;
+                    up = false;
+                }
+            }
+            if (xy <= limit) {
+                if (y < 0) {
+                    up =true;
+                    down=false;
+                    left=false;
+                    right = false;  
+                } else {
+                    down = true;
+                    up=false;
+                    left=false;
+                    right = false;
+                }
+            }
+        } else {
+            shoot = true;
+            if(up == true){
+                shootup = true;
+            }
+            if(down == true){
+                shootdown = true;
+            }
+            if(left == true){
+                shootleft = true;
+            }
+            if(right == true){
+                shootright = true;
+            }
+        }
+    }
+
     window.addEventListener ('keypress', function(event)
     { 
         if(event.keyCode == 32){
